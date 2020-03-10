99c60c1b0db0ad1ae25b4d9085c47d7b9ff7092e
Fabio Vesperini <f.vesperini@mflgroup.com>
2020-03-10 11:58:57 +0100

    upd

--- VGG_feat_extract.py
+++ VGG_feat_extract.py
@@ -1,52 +1,60 @@
 import os
-from keras.applications.vgg19 import VGG19
-from keras.preprocessing import image
-from keras.applications.vgg19 import preprocess_input
-from keras.models import Model
+from matplotlib import pyplot as plt
+from tensorflow.python.keras.applications.vgg19 import VGG19
+from tensorflow.python.keras.preprocessing import image
+from tensorflow.python.keras.applications.vgg19 import preprocess_input
+from tensorflow.python.keras.models import Model
 import numpy as np
 
 base_model = VGG19(weights='imagenet')
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('flatten').output)
 
+
+# def get_features(img_path):
+#     img = image.load_img(img_path, target_size=(224, 224))
+#     x = image.img_to_array(img)
+#     x = np.expand_dims(x, axis=0)
+#     x = preprocess_input(x)
+#     flatten = model.predict(x)
+#     return list(flatten[0])
+
 def get_features(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    data = np.load(img_path)
+    plt.imshow(data, interpolation='nearest')
+    plt.show()
+    plt.savefig(img_path + '.png')
+    # x = image.img_to_array(img)
+    # x = np.expand_dims(x, axis=0)
+    x = preprocess_input(data)
     flatten = model.predict(x)
-    return list(flatten[0])
+    # return list(flatten[0])
+    return 0
+
 
 X = []
 y = []
+features_path = 'dataset/128Logmel+Deltas'
 
-car_plots = []
-for (_,_,filenames) in os.walk('carPlots'):
-    car_plots.extend(filenames)
+logmels = []
+for (_, _, filenames) in os.walk(features_path):
+    logmels.extend(filenames)
     break
 
-for cplot in car_plots:
-    X.append(get_features('carPlots/' + cplot))
+for lm in logmels:
+    X.append(get_features(os.path.join(features_path, lm)))
     y.append(0)
 
-bike_plots = []
-for (_,_,filenames) in os.walk('bikePlots'):
-    bike_plots.extend(filenames)
-    break
 
-for cplot in bike_plots:
-    X.append(get_features('bikePlots/' + cplot))
-    y.append(1)
-
-from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42, stratify=y)
-
-clf = LinearSVC(random_state=0, tol=1e-5)
-clf.fit(X_train, y_train)
-
-predicted = clf.predict(X_test)
-
-# get the accuracy
-print (accuracy_score(y_test, predicted))
+# from sklearn.model_selection import train_test_split
+# from sklearn.svm import LinearSVC
+# from sklearn.metrics import accuracy_score
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42, stratify=y)
+#
+# clf = LinearSVC(random_state=0, tol=1e-5)
+# clf.fit(X_train, y_train)
+#
+# predicted = clf.predict(X_test)
+#
+# # get the accuracy
+# print (accuracy_score(y_test, predicted))
