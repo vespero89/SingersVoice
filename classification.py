0a8b8e45de3c851bddd4a0e4c2e75eb0ac33b2f7
Fabio Vesperini <f.vesperini@mflgroup.com>
2020-03-11 14:17:06 +0100

    ready to test

---
+++ classification.py
@@ -0,0 +1,42 @@
+import numpy as np
+import os
+import pandas as pd
+import sklearn
+
+
+def get_features(f_name):
+    vector = np.load(f_name + '.npy')
+    return vector
+
+
+def get_labels(string):
+    lab = 0
+    if string == 'PRE':
+        lab = 0
+    elif string == 'POST':
+        lab = 1
+    return lab
+
+
+feat_type = 'predictions'
+classifier = 'VGG19'
+features_path = os.path.join('dataset', feat_type, classifier)
+metadata_file = 'metadata.csv'
+
+data = pd.read_csv(metadata_file, header=0, sep=',', names=['filename', 'singer', 'class', 'date_rec'])
+
+y = []
+for file in data.iterrows():
+    filename, _ = os.path.splitext(file[1]['filename'])
+    label = get_labels(file[1]['class'])
+    features_tmp = get_features(os.path.join(features_path, filename))
+    y.append(label)
+
+    if 'X' in locals():
+        X = np.concatenate((X, features_tmp))
+    else:
+        X = features_tmp
+
+print('Done')
+
+
